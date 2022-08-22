abb3c68d39dc6b6a0fbd12ecbc8ec027b7a03a39
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-22 21:45:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+names=["Guadalupe", "Ollie", "Aki"];
+message="surprise" 
+function writeCards(names,message) {
+  const newArray=[]
+   
+  for (let i = 0; i < names.length; i++) {
+    
+  const msg=`Thank you, ${names[i]}, for the wonderful ${message} gift!`;
+    newArray.push(msg);
+}
+ return newArray;
+}
+writeCards(names,message)
+
+function countDown(i){
+  while(i>=0){
+    console.log(i);
+    i--;
+  }
+}
